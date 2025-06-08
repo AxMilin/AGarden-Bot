d639e581c0eac797350feda5d348f9fb8b14fb2e
Jaruwit A. <151182569+AxMilin@users.noreply.github.com>
2025-06-08 18:27:37 +0700

    Update stocks.js

--- Web/functions/stocks.js
+++ Web/functions/stocks.js
@@ -1,6 +1,13 @@
 export async function onRequestGet() {
   try {
-    const res = await fetch("http://65.108.103.151:22377/api/stocks");
+    const res = await fetch("http://65.108.103.151:22377/api/stocks", {
+      headers: {
+        "User-Agent": "curl/8.0",
+        "Accept": "application/json",
+        "Referer": "http://65.108.103.151:22377",
+        "Origin": "http://65.108.103.151:22377"
+      }
+    });
 
     if (!res.ok) {
       return new Response(`Backend error: ${res.status}`, { status: 502 });
@@ -11,7 +18,7 @@ export async function onRequestGet() {
     return new Response(JSON.stringify(data), {
       headers: {
         "Content-Type": "application/json",
-        "Cache-Control": "no-cache"
+        "Cache-Control": "no-store"
       }
     });
   } catch (err) {
